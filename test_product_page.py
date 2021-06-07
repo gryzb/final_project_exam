59e7aa67b3a5dd373d8132ecb9003cbeb5b17397
Andrew <dekin1337@gmail.com>
2021-06-07 20:40:49 +0300

    Update test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -42,13 +42,14 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.go_to_login_page()
 
 @pytest.mark.need_review
-def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = BasketPage(browser,link)
+    page = ProductPage(browser,link)
     page.open()
     page.go_to_basket_page()
-    page.should_be_success_message_basket()
-    page.should_not_be_success_message_basket()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_be_empty_basket_message()
+    basket_page.should_be_empty_basket()
 
 @pytest.mark.test_test
 class TestUserAddToBasketFromProductPage():
